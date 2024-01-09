1d2431467855eb8972fe32b8f8ea7f3678d7444a
Akhmad Salafudin <axmad386@gmail.com>
2024-01-09 23:22:24 +0700

    feat(migration): messages table

---
+++ database/migrations/2024_01_09_162017_create_messages_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('from_user', 32);
+            $table->string('to_user', 32);
+            $table->string('title', 60);
+            $table->mediumText('message');
+            $table->enum('status', [0, 1])->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
