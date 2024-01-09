c8dfeed9a3f5d9fa2c080ac03670ce94bab0b230
Akhmad Salafudin <axmad386@gmail.com>
2024-01-10 00:54:48 +0700

    feat(migration): radusergroups table

---
+++ database/migrations/2024_01_09_175350_create_radusergroups_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('radusergroups', function (Blueprint $table) {
+            $table->id();
+            $table->string('username', 64)->default('');
+            $table->string('groupname', 64)->default('');
+            $table->integer('priority')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('radusergroups');
+    }
+};
