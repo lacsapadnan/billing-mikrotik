aed376564ad0980bf6af3f6a09eb5ad5197d9e8b
Akhmad Salafudin <axmad386@gmail.com>
2024-01-09 23:57:43 +0700

    feat(migration): pools table

---
+++ database/migrations/2024_01_09_165648_create_pools_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pools', function (Blueprint $table) {
+            $table->id();
+            $table->string('pool_name', 40);
+            $table->string('range_ip', 40);
+            $table->string('routers', 40);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pools');
+    }
+};
