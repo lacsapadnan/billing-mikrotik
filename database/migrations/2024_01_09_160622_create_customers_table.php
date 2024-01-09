ad5f39cb20a9a9ed1daf0b7070d5a99aef0387a7
Akhmad Salafudin <axmad386@gmail.com>
2024-01-09 23:13:32 +0700

    feat(migration): customers table

---
+++ database/migrations/2024_01_09_160622_create_customers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Enum\ServiceType;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('username', 45);
+            $table->string('password');
+            $table->string('pppoe_password')->comment('For PPPOE Login');
+            $table->string('fullname', 45);
+            $table->mediumText('address');
+            $table->string('phonenumber', 20);
+            $table->string('email', 128);
+            $table->decimal('balance', 15, 2)->comment('For Money Deposit');
+            $table->enum('service_type', array_column(ServiceType::cases(), 'name'))->comment('for selecting user type');
+            $table->boolean('auto_renewal')->comment('Auto renewall using balance');
+            $table->dateTime('last_login')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
