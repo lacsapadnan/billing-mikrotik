013fd1e96709fb9acde2bc63b584cedef9e8abfc
Akhmad Salafudin <axmad386@gmail.com>
2024-01-10 00:52:06 +0700

    feat(migration): radpostauths table

---
+++ database/migrations/2024_01_09_174946_create_radpostauths_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('radpostauths', function (Blueprint $table) {
+            $table->id();
+            $table->string('username', 64)->default('');
+            $table->string('pass', 64)->default('');
+            $table->string('reply', 32)->default('');
+            $table->timestamp('authdate', 6)->useCurrent();
+            $table->string('class', 64)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('radpostauths');
+    }
+};
