e15c2a9cb80f965ef6d57d25cc625fb54cbf29f7
Akhmad Salafudin <axmad386@gmail.com>
2024-01-10 00:48:10 +0700

    feat(migration): radgroupreplies table

---
+++ database/migrations/2024_01_09_174700_create_radgroupreplies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('radgroupreplies', function (Blueprint $table) {
+            $table->id();
+            $table->string('groupname', 64)->default('');
+            $table->string('attribute', 64)->default('');
+            $table->string('op', 2)->default('==');
+            $table->string('value', 253)->default('');
+            $table->foreignId('plan_id')->constrained('plans');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('radgroupreplies');
+    }
+};
