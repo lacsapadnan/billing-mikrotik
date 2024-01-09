1af24b841a8753f715c4d30b62520505bd6a0425
Akhmad Salafudin <axmad386@gmail.com>
2024-01-10 00:44:58 +0700

    feat(migration): radchecks table

---
+++ database/migrations/2024_01_09_174327_create_radchecks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('radchecks', function (Blueprint $table) {
+            $table->id();
+            $table->string('username', 64)->default('');
+            $table->string('attribute', 64)->default('');
+            $table->string('op', 2)->default('==');
+            $table->string('value', 253)->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('radchecks');
+    }
+};
