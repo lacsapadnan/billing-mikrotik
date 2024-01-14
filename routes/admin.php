5c07fe46ed623e60968c899679d90c9cb0050e0c
Akhmad Salafudin <axmad386@gmail.com>
2024-01-14 17:27:49 +0700

    refactor(admin-customer): ganti ke resource controller

--- routes/admin.php
+++ routes/admin.php
@@ -14,15 +14,6 @@ Route::name('admin:')->group(function () {
         Route::view('dashboard', 'admin.dashboard')->name('dashboard');
         Route::get('logout', [AdminAuthController::class, 'destroy'])->name('auth.logout');
         Route::resource('customer', AdminCustomerController::class);
-        // Route::prefix('customer')->name('customer.')->group(function () {
-        //     Route::get('/', [AdminCustomerController::class, 'index'])->name('list');
-        //     Route::get('/{customer}/detail', [AdminCustomerController::class, 'show'])->name('detail');
-        //     Route::get('/{customer}/delete', [AdminCustomerController::class, 'destroy'])->name('delete');
-        //     Route::get('/add', [AdminCustomerController::class, 'create'])->name('add');
-        //     Route::get('/{customer}/edit', [AdminCustomerController::class, 'edit'])->name('edit');
-        //     Route::post('/', [AdminCustomerController::class, 'store'])->name('store');
-        //     Route::post('/{customer}', [AdminCustomerController::class, 'update'])->name('update');
-        // });
     });
     Route::redirect('/', '/admin/dashboard', 301);
 });
