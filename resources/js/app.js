71e11340ba0c96a9a5be6060386175e88776b313
Akhmad Salafudin <axmad386@gmail.com>
2024-01-17 22:24:46 +0700

    refactor: confirmable diganti menjadi alpine component

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,26 +6,27 @@ window.Alpine = Alpine;
 
 Alpine.start();
 
-$(".confirmable").click(function(e) {
-    e.preventDefault();
-    const data = $(this).data();
-    Swal.fire({
-        title: data.confirmTitle || "Are you sure?",
-        text: data.confirmText || "You won't be able to revert this!",
-        icon: "warning",
-        showCancelButton:
-            typeof data.showCancelButton == "undefined"
-                ? true
-                : data.showCancelButton == "true",
-        confirmButtonColor: data.confirmButtonColor,
-        cancelButtonColor: data.cancelButtonColor,
-        confirmButtonText: data.confirmButtonText || "Yes!",
-    }).then((result) => {
-        if (result.isConfirmed) {
-            eval(data.onConfirm);
-        }
-    });
-});
+Alpine.data("confirmable", (data) => ({
+    confirm(e) {
+        e.preventDefault();
+        Swal.fire({
+            title: data.confirmTitle || "Are you sure?",
+            text: data.confirmText || "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton:
+                typeof data.showCancelButton == "undefined"
+                    ? true
+                    : data.showCancelButton == "true",
+            confirmButtonColor: data.confirmButtonColor,
+            cancelButtonColor: data.cancelButtonColor,
+            confirmButtonText: data.confirmButtonText || "Yes!",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                data.onConfirm()
+            }
+        });
+    },
+}));
 
 // Element to indecate
 var button = document.querySelector("#kt_button_toggle");
