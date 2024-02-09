116f0a847dd20e02c72013bfec9ae70d54b165d4
Akhmad Salafudin <axmad386@gmail.com>
2024-02-10 03:57:59 +0700

    feat(database): migration dan seeder pages

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserSeeder::class);
         $this->call(ConfigSeeder::class);
+        $this->call(PageSeeder::class);
     }
 }
---
+++ database/seeders/PageSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $pages = [
+            'Announcement',
+            'Order_Voucher',
+            'Privacy_Policy',
+            'Registration_Info',
+            'Terms_and_Conditions',
+            'Voucher'
+        ];
+        foreach($pages as $title){
+            $content = file_get_contents(database_path("seeders/raw/html/$title.html"));
+            \App\Models\Page::create([
+                'title' => $title,
+                'content' => $content
+            ]);
+        }
+    }
+}
