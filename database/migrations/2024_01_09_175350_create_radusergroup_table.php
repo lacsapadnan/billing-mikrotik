d01fd516bbcf76b5c2ec9c4011a7354053d89460
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2024-03-12 21:08:19 +0700

    refactor(radusergroup migration): change from plural to singular table name

--- database/migrations/2024_01_09_175350_create_radusergroup_table.php
+++ database/migrations/2024_01_09_175350_create_radusergroup_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('radusergroups', function (Blueprint $table) {
+        Schema::create('radusergroup', function (Blueprint $table) {
             $table->id();
             $table->string('username', 64)->default('')->index();
             $table->string('groupname', 64)->default('');
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('radusergroups');
+        Schema::dropIfExists('radusergroup');
     }
 };
