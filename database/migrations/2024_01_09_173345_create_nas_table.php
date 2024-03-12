4f17f3a6ab94f1c3e1a9b9f1dd7316c28525f7b9
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2024-03-12 21:07:01 +0700

    refactor(nas migration): change from plural to singular table name

--- database/migrations/2024_01_09_173345_create_nas_table.php
+++ database/migrations/2024_01_09_173345_create_nas_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('nasses', function (Blueprint $table) {
+        Schema::create('nas', function (Blueprint $table) {
             $table->id();
             $table->string('nasname', 128)->index();
             $table->string('shortname', 32)->nullable();
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('nasses');
+        Schema::dropIfExists('nas');
     }
 };
