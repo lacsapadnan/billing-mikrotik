90da3007dff5b201fb2e659d579ae29b4f9b02f8
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2024-03-12 21:07:45 +0700

    refactor(radgroupcheck migration): change from plural to singular table name

--- database/migrations/2024_01_09_174516_create_radgroupcheck_table.php
+++ database/migrations/2024_01_09_174516_create_radgroupcheck_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('radgroupchecks', function (Blueprint $table) {
+        Schema::create('radgroupcheck', function (Blueprint $table) {
             $table->id();
             $table->string('groupname', 64)->default('')->index();
             $table->string('attribute', 64)->default('');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('radgroupchecks');
+        Schema::dropIfExists('radgroupcheck');
     }
 };
