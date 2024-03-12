e0e9a8a60bacd9828828a2890ce849ada4872fcd
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2024-03-12 21:07:53 +0700

    refactor(radgroupreply migration): change from plural to singular table name

--- database/migrations/2024_01_09_174700_create_radgroupreply_table.php
+++ database/migrations/2024_01_09_174700_create_radgroupreply_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('radgroupreplies', function (Blueprint $table) {
+        Schema::create('radgroupreply', function (Blueprint $table) {
             $table->id();
             $table->string('groupname', 64)->default('')->index();
             $table->string('attribute', 64)->default('');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('radgroupreplies');
+        Schema::dropIfExists('radgroupreply');
     }
 };
